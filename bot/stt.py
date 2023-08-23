6ef42d7a2e80e3cee1d9acc24d9ecdea9791ec54
creamsandwch <neuronrus1996@gmail.com>
2023-08-23 12:57:56 +0500

    speech recognition with external API added

---
+++ bot/stt.py
@@ -0,0 +1,12 @@
+import speech_recognition as sr
+
+
+def audio_to_text(dest_name: str):
+    """Translates .wav to text."""
+    r = sr.Recognizer()
+    message = sr.AudioFile(dest_name)
+    with message as source:
+        audio = r.record(source)
+
+    result = r.recognize_google(audio, language='ru_RU')
+    return result
